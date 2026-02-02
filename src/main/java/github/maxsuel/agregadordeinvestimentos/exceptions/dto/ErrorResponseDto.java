7a80d20250925c0a7740834513de9642e8733bff
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2026-02-02 11:40:07 -0300

    feat: add not null on schemas

--- src/main/java/github/maxsuel/agregadordeinvestimentos/exceptions/dto/ErrorResponseDto.java
+++ src/main/java/github/maxsuel/agregadordeinvestimentos/exceptions/dto/ErrorResponseDto.java
@@ -1,17 +1,22 @@
 package github.maxsuel.agregadordeinvestimentos.exceptions.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.Instant;
 import java.util.Map;
 
 @Schema(description = "Standardized object for returning API error details.")
 public record ErrorResponseDto(
+        @NotNull
         @Schema(example = "400", description = "HTTP Status Code")
         int status,
 
+        @NotNull
         @Schema(example = "Validation failed for one or more fields", description = "General error message")
         String message,
 
+        @NotNull
         @Schema(description = "Timestamp when the error occurred")
         Instant timestamp,
 
